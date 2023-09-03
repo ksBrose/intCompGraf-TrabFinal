a49773c17e21406c7fb139cf191d9292b81b8729
Kelvin Brose <ksb_56@hotmail.com>
2023-09-03 00:56:04 -0300

    adicionado camera look at
    apertando "c" muda de camera livre para look at center (centro da sala)
    apertando novamente volta para camera livre

--- src/main.cpp
+++ src/main.cpp
@@ -269,6 +269,7 @@ bool reset = false;
 float tetay =0;
 glm::vec3 bezier_cintura;
 /**Animação add**/
+bool camera_type = false;
 
 int main(int argc, char* argv[])
 {
@@ -416,7 +417,7 @@ int main(int argc, char* argv[])
     while (!glfwWindowShouldClose(window))
     {
         // Aqui executamos as operações de renderização
-
+        glm::mat4 view;
         // Definimos a cor do "fundo" do framebuffer como branco.  Tal cor é
         // definida como coeficientes RGBA: Red, Green, Blue, Alpha; isto é:
         // Vermelho, Verde, Azul, Alpha (valor de transparência).
@@ -450,44 +451,66 @@ int main(int argc, char* argv[])
         // comentários detalhados dentro da definição de BuildTriangles().
         glBindVertexArray(vertex_array_object_id);
 /**Boneco add**/
-/*
+
         // Computamos a posição da câmera utilizando coordenadas esféricas.  As
         // variáveis g_CameraDistance, g_CameraPhi, e g_CameraTheta são
         // controladas pelo mouse do usuário. Veja as funções CursorPosCallback()
         // e ScrollCallback().
-        float r = g_CameraDistance;
-        float y = r*sin(g_CameraPhi);
-        float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
-        float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
-*//**free camera add**/
+        //if(camera_type){
         //float r = g_CameraDistance;
+        //float y = r*sin(g_CameraPhi);
+        //float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
+        //float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
+        //}
+//**free camera add**/
+        //float r = g_CameraDistance;
+        if(!camera_type)
+        {
         float y = sin(g_Phi);
         float z = cos(g_Phi)*cos(g_Theta);
         float x = cos(g_Phi)*sin(g_Theta);
-         camera_view_vector  = glm::vec4(x,-y,z,0.0f);
+        camera_view_vector  = glm::vec4(x,-y,z,0.0f);
+        }
+
 /**free camera add**/
-/*
+        if(camera_type)
+        {
+        float r = g_CameraDistance;
+        float y = r*sin(g_Phi);
+        float z = r*cos(g_Phi)*cos(g_Theta);
+        float x = r*cos(g_Phi)*sin(g_Theta);
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
         // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
+        glm::vec4 camera_position_c  = glm::vec4(-x,y,-z,1.0f); // Ponto "c", centro da câmera
         glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
         glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
         glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
-*/
+
+        view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+
+        }
 /**free camera add**/
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
         // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
         //glm::vec4 camera_position_c  = glm::vec4(1.0f,1.0f,1.0f,1.0f); // Ponto "c", centro da câmera
         //glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
        // glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
+        glm::vec4 w;
+        glm::vec4 u;
+        if(!camera_type)
+        {
         glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
-        glm::vec4 w = -camera_view_vector;
-        glm::vec4 u = crossproduct(camera_up_vector,-camera_view_vector);
+         w = -camera_view_vector;
+         u = crossproduct(camera_up_vector,-camera_view_vector);
+
+         view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+
+        }
 /**free camera add**/
 
         // Computamos a matriz "View" utilizando os parâmetros da câmera para
         // definir o sistema de coordenadas da câmera.  Veja slides 2-14, 184-190 e 236-242 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+//        glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
 
         // Agora computamos a matriz de Projeção.
         glm::mat4 projection;
@@ -2280,7 +2303,10 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
     {
         g_AngleZ += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
     }
-
+    if (key == GLFW_KEY_C && action == GLFW_PRESS)
+    {
+        camera_type= !camera_type;
+    }
     // Se o usuário apertar a tecla espaço, resetamos os ângulos de Euler para zero.
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
     {
